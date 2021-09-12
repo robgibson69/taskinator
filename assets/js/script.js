853f318037b6a705cbdb63d83e16d439581a5c50
GIBBY <git_gibby@bulldogsolutions.ca>
2021-09-12 12:53:47 -0400

    added taskHandler to add tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,21 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    // add item info to the task-item div
+    listItemEl.appendChild(taskInfoEl);
+    // add above to the ul, tasks-to-do
     tasksToDoEl.appendChild(listItemEl);
 };
 
